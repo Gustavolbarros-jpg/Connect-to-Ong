6fac6aa6f09b0b6cccf8d9a74d99afd646cf29a4
Isaac Teixeira <isaac.manoel06@gmail.com>
2025-06-22 18:03:29 -0300

    dependencias

---
+++ backend/src/index.js
@@ -0,0 +1,27 @@
+import express from 'express'
+import cors from "cors"
+
+async function main() {
+    const hostname = 'localhost'
+    const port = 3000
+
+    const app = express()
+
+    app.use(express.json())
+    app.use(cors())
+
+    app.get('/', (req, res) => {
+        res.send({
+            sucess: true,
+            statusCode: 200,
+            body: 'Welcome to Connect to Ong'
+        })
+    })
+
+    app.listen(port, () => {
+        console.log(`Server running on: http://${hostname}:${port}`)
+    })
+
+}
+
+main()
