70743f8f9b29737322ff5f771a38c72411995091
Wallace Gabriel <wgsl@cin.ufpe.br>
2025-07-18 00:09:49 -0300

    Adcionando no banco descrição do user e link de foto de perfil

--- backend/src/dominio/users.js
+++ backend/src/dominio/users.js
@@ -1,5 +1,5 @@
 import { Mongo } from "../database/mongo.js"; 
-import { ObjectId } from "mongodb";
+import { ObjectId, ReturnDocument } from "mongodb";
 import crypto from 'crypto'
 import { error } from "console";
 
@@ -27,6 +27,9 @@ export default class UsersDataAcess {
 
 
     async updateUsers(userId, userData){
+        // Filtro para adcionar coisas permitidas
+        const allowedFields = ['description', 'photUrl']
+        // ^
         if(userData.password){
             const salt=crypto.randomBytes(16)
                 crypto.pbkdf2(userData.password,salt,310000,16,'sha256',async(error,hashedPassword)=>{
@@ -49,10 +52,21 @@ export default class UsersDataAcess {
 
         }
         else{
-            const result = await Mongo.db 
+        const filteredData = {}
+        for (const field of allowedFields){
+            if (userData[field] !== undefined){
+                filteredData[field] = userData[field]
+            }
+        }
+        if (Object.keys(filteredData).length === 0) {
+            throw new Error("Nenhum campo válido para atualizar.");
+        }
+        const result = await Mongo.db 
         .collection(collectionName)
-        .findOneAndUpdate({_id: new ObjectId(userId)},
-        {$set:userData} )
+        .findOneAndUpdate(
+        {_id: new ObjectId(userId)},
+        {$set:filteredData}
+    )
          return result
         }
     
