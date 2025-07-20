2fde33f7b182d242e0b6eede047465ebd9b9525e
Emanoel <eemm@cin.ufpe.br>
2025-07-20 19:53:18 -0300

    CHORE-WIP: Trabalho em progresso.

--- frontend/src/Pags/OngPage/OngPage.jsx
+++ frontend/src/Pags/OngPage/OngPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 
 import Navbar from "../../Components/Navbar/";
+import Footer from "../../Components/Footer/";
 
 function OngPage() {
     return (
@@ -12,9 +13,9 @@ function OngPage() {
                             <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
                                 <img src="" alt="" className="h-5 w-5"/>
                             </div>
-                            <input type="text" placeholder="Você tem interesse em uma ONG's específica, pesquise aqui..." className="w-full text-xs md:text-base p-3 pl-10 border border-gray-300 rounded-[4px] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"/>
+                            <input type="text" placeholder="Você tem interesse em uma ONG's específica, pesquise aqui..." className="w-full text-xs md:text-[20px] p-3 pl-10 border border-gray-300 rounded-[4px] focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"/>
                         </div>
-                        <div className="flex flex-col md:flex-row md:items-center justify-between md:gap-4">
+                        <div className="flex flex-col md:flex-row md:items-center justify-between md:gap-4 md:text-[20px]">
                             <div className="flex flex-col md:flex-row gap-3 w-full md:w-auto">
                                 <select className="form-select border border-gray-300 rounded-[4px] py-2 px-3 focus:outline-none focus:ring-2 focus:ring-blue-500 w-full md:w-auto">
                                     <option>Área de Atuação</option>
@@ -36,6 +37,7 @@ function OngPage() {
                         </div>
                 </section>
             </main>
+            <Footer />
         </div>
     )
 }
