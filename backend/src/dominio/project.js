fa550e6a7a1fb608049dccd98440a8c565a5aacb
Isaac Teixeira <isaac.manoel06@gmail.com>
2025-07-24 16:02:49 -0300

    feat: classe projeto

--- backend/src/dominio/project.js
+++ backend/src/dominio/project.js
@@ -3,33 +3,51 @@ import { ObjectId } from "mongodb";
 
 const collectionName ='project'
 
-export default class ProjectExtension {
+export default class ProjectDataAccess {
 
-
-    async createProject(ProjectData) {
+    async createProject(projectData) {
         const result = await Mongo.db
         .collection(collectionName)
-        .insertOne(ProjectData)
+        .insertOne(projectData)
+
+    return result.insertedId
+    }
+
+    async getProjectById(projectId) {
+        const project = await Mongo.db
+        .collection(collectionName)
+        .findOne({ _id: new ObjectId(projectId) })
+
+        return project
+    }
+
+    async getAllProjects() {
+        const projects = await Mongo.db
+        .collection(collectionName)
+        .find({})
+        .toArray()
+
+        return projects
+    }
+
+    async deleteProject(projectId) {
+        const result = await Mongo.db
+            .collection(collectionName)
+            .deleteOne({ _id: new ObjectId(projectId) })
 
         return result
-
-
     }
 
-    
+    async updateProject(projectId, updateData) {
+        const result = await Mongo.db
+            .collection(collectionName)
+            .updateOne(
+                { _id: new ObjectId(projectId) }, 
+                { $set: updateData }              
+            )
 
-
-
-
-    async getProject() {
-
+        return result
     }
-
-    async deleteProject() {
-
-    }
-
-
 
 }
 
