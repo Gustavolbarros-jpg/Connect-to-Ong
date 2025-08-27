d436a4d9e06db84b3149fb1b2da5a9ccfb252be7
Vinicius Lidington <vlidington@gmail.com>
2025-08-26 22:18:04 -0300

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/connecttoong/", // Caminho base para o frontend
+  preview: {
+    port: 3017, // Porta do frontend no ambiente de preview
+    strictPort: true,
+  },
+  server: {
+    port: 3017, // Porta do servidor de desenvolvimento
+    strictPort: true,
+    host: true, // Permite acesso externo
+    origin: "http://0.0.0.0:3017", // Origem para o servidor
+  },
 });
